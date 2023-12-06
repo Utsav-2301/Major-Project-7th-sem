418b1e0cc6b64b00a50b340e5833ead880e23d5e
Utsav Sharma <utsav.dhariwal@gmail.com>
2023-12-06 12:37:58 +0530

    script updated for download button

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -141,37 +141,19 @@ const HomePage = () => {
       .catch((error) => console.error("Error:", error));
   }
 
-  const stopPlotting = () =>{
-    fetch("http://localhost:5000/stop-plotting", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error closing the connection");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => console.error("Error:", error));
-  }
 
   const downloadData = () => {
-    fetch("/download-data")
+    fetch('http://localhost:5000/download-data')
       .then((response) => response.blob())
       .then((blob) => {
-        const link = document.createElement("a");
-        link.href = URL.createObjectURL(blob);
-        link.download = "voltage_data.txt";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(blob)
+        link.download = 'data.txt'
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
       })
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => console.error('Error:', error))
   };
 
   return (
@@ -324,11 +306,11 @@ const HomePage = () => {
           handleClick={() => newConnection()}
           disabled={false}
         />
-        <CustomButton
+        {/* <CustomButton
           text="Stop Plotting"
           handleClick={() => stopPlotting()}
           disabled={false}
-        />
+        /> */}
         <CustomButton
           text="Download Data"
           handleClick={() => downloadData()}
